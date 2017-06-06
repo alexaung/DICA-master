85d8c8a3046887cfa08ec40179368f9912358329
zayar <zayarmin@gmail.com>
2017-06-06 21:45:26 +0630

    Redirect To Investment as home

--- dica/Controllers/AccountController.cs
+++ dica/Controllers/AccountController.cs
@@ -449,7 +449,7 @@ namespace dica.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Investment");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
