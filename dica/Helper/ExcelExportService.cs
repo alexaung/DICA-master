afb66181875f8a6af9981fc0d0293b4e884faa7b
alexaung <aungmo@gmail.com>
2017-06-13 19:25:07 +0630

    Added Cell Border

--- dica/Helper/ExcelExportService.cs
+++ dica/Helper/ExcelExportService.cs
@@ -59,12 +59,42 @@ namespace dica.Helper
             var styleTitle = wb.CreateCellStyle();
             styleTitle.SetFont(font);
             styleTitle.Alignment = HorizontalAlignment.Right;
+            styleTitle.BorderTop = BorderStyle.Double;
+            styleTitle.BorderBottom = BorderStyle.Double;
+            styleTitle.BorderLeft = BorderStyle.Double;
+            styleTitle.BorderRight = BorderStyle.Double;
+            styleTitle.TopBorderColor = HSSFColor.Black.Index;
+            styleTitle.BottomBorderColor = HSSFColor.Black.Index;
+            styleTitle.LeftBorderColor = HSSFColor.Black.Index;
+            styleTitle.RightBorderColor = HSSFColor.Black.Index;
             styles.Add("Title", styleTitle);
+
+            var styleSubTitle = wb.CreateCellStyle();
+            styleSubTitle.SetFont(font);
+            styleSubTitle.Alignment = HorizontalAlignment.Center;
+            styleSubTitle.BorderTop = BorderStyle.Double;
+            styleSubTitle.BorderBottom = BorderStyle.Double;
+            styleSubTitle.BorderLeft = BorderStyle.Double;
+            styleSubTitle.BorderRight = BorderStyle.Double;
+            styleSubTitle.TopBorderColor = HSSFColor.Black.Index;
+            styleSubTitle.BottomBorderColor = HSSFColor.Black.Index;
+            styleSubTitle.LeftBorderColor = HSSFColor.Black.Index;
+            styleSubTitle.RightBorderColor = HSSFColor.Black.Index;
+            styles.Add("SubTitle", styleSubTitle);
 
             var styleHeader = wb.CreateCellStyle();
             styleHeader.SetFont(font);
             styleHeader.Alignment = HorizontalAlignment.Center;
-            styleHeader.WrapText = true;
+            styleHeader.WrapText = false;
+            styleHeader.BorderTop = BorderStyle.Double;
+            styleHeader.BorderBottom = BorderStyle.Double;
+            styleHeader.BorderLeft = BorderStyle.Double;
+            styleHeader.BorderRight = BorderStyle.Double;
+            styleHeader.TopBorderColor = HSSFColor.Black.Index;
+            styleHeader.BottomBorderColor = HSSFColor.Black.Index;
+            styleHeader.LeftBorderColor = HSSFColor.Black.Index;
+            styleHeader.RightBorderColor = HSSFColor.Black.Index;
+            styleHeader.FillBackgroundColor = HSSFColor.Grey25Percent.Index;
             styles.Add("Header", styleHeader);
 
             var styleDateTime = wb.CreateCellStyle();
@@ -77,6 +107,14 @@ namespace dica.Helper
 
             var style2Decimal = wb.CreateCellStyle();
             style2Decimal.DataFormat = dataFormat.GetFormat("0.00"); // dataFormat.GetFormat("#0.00");
+            style2Decimal.BorderTop = BorderStyle.Double;
+            style2Decimal.BorderBottom = BorderStyle.Double;
+            style2Decimal.BorderLeft = BorderStyle.Double;
+            style2Decimal.BorderRight = BorderStyle.Double;
+            style2Decimal.TopBorderColor = HSSFColor.Black.Index;
+            style2Decimal.BottomBorderColor = HSSFColor.Black.Index;
+            style2Decimal.LeftBorderColor = HSSFColor.Black.Index;
+            style2Decimal.RightBorderColor = HSSFColor.Black.Index;
             styles.Add("2Decimal", style2Decimal);
 
             var style1Decimal = wb.CreateCellStyle();
@@ -86,7 +124,14 @@ namespace dica.Helper
             var styleGeneral = wb.CreateCellStyle();
             styleGeneral.DataFormat = dataFormat.GetFormat("General");
             styleGeneral.WrapText = false;
+            styleGeneral.BorderTop = BorderStyle.Double;
+            styleGeneral.BorderBottom = BorderStyle.Double;
+            styleGeneral.BorderLeft = BorderStyle.Double;
+            styleGeneral.BorderRight = BorderStyle.Double;
+            styleGeneral.TopBorderColor = HSSFColor.Black.Index;
             styleGeneral.BottomBorderColor = HSSFColor.Black.Index;
+            styleGeneral.LeftBorderColor = HSSFColor.Black.Index;
+            styleGeneral.RightBorderColor = HSSFColor.Black.Index;
             styles.Add("General", styleGeneral);
 
             return styles;
@@ -137,10 +182,15 @@ namespace dica.Helper
             //Create a header row
             var row = sheet.CreateRow(0);
 
-            var cell = row.CreateCell(0);
-            cell.CellStyle = styles["Title"];
-            cell.SetCellValue("This is a Titel");
-
+            for (int i = 0; i <= (data.Sectors.Count * 2) + 1; ++i)
+            {
+                var cell = row.CreateCell(i);
+                cell.CellStyle = styles["Title"];
+                if (i == 0)
+                {
+                    cell.SetCellValue("This is a Titel");
+                }
+            }
             sheet.AddMergedRegion(new CellRangeAddress(
                     0, //first row (0-based)
                     0, //last row  (0-based)
@@ -148,11 +198,18 @@ namespace dica.Helper
                     (data.Sectors.Count * 2) + 1  //last column  (0-based)
             ));
 
+            
             row = sheet.CreateRow(1);
 
-            cell = row.CreateCell(0);
-            cell.CellStyle = styles["Header"];
-            cell.SetCellValue("This is a Title");
+            for (int i = 0; i <= (data.Sectors.Count * 2) + 1; ++i)
+            {
+                var cell = row.CreateCell(i);
+                cell.CellStyle = styles["SubTitle"];
+                if (i == 0)
+                {
+                    cell.SetCellValue("This is a Titel");
+                }
+            }
 
             sheet.AddMergedRegion(new CellRangeAddress(
                     1, //first row (0-based)
@@ -163,10 +220,15 @@ namespace dica.Helper
 
             row = sheet.CreateRow(2);
 
-            cell = row.CreateCell(0);
-            cell.CellStyle = styles["Title"];
-            //cell.CellStyle.Alignment = HorizontalAlignment.Right;
-            cell.SetCellValue("This is a Title");
+            for (int i = 0; i <= (data.Sectors.Count * 2) + 1; ++i)
+            {
+                var cell = row.CreateCell(i);
+                cell.CellStyle = styles["Title"];
+                if (i == 0)
+                {
+                    cell.SetCellValue("This is a Titel");
+                }
+            }
 
             sheet.AddMergedRegion(new CellRangeAddress(
                     2, //first row (0-based)
@@ -183,26 +245,21 @@ namespace dica.Helper
             cell.CellStyle = styles["Header"];
             cell.SetCellValue("#");
 
-            sheet.AddMergedRegion(new CellRangeAddress(
-                    3, //first row (0-based)
-                    4, //last row  (0-based)
-                    0, //first column (0-based)
-                    0  //last column  (0-based)
-            ));
+            
 
             cell = row.CreateCell(1);
             cell.CellStyle = styles["Header"];
             cell.SetCellValue("Country");
 
-            sheet.AddMergedRegion(new CellRangeAddress(
-                    3, //first row (0-based)
-                    4, //last row  (0-based)
-                    1, //first column (0-based)
-                    1  //last column  (0-based)
-            ));
+            
 
             var columNo = 2;
             data.Sectors.ForEach(s=> {
+                var firstColumnNo = columNo;
+                cell = row.CreateCell(columNo++);
+                cell.CellStyle = styles["Header"];
+                cell.SetCellValue(s.Name);
+
                 cell = row.CreateCell(columNo);
                 cell.CellStyle = styles["Header"];
                 cell.SetCellValue(s.Name);
@@ -210,12 +267,19 @@ namespace dica.Helper
                 sheet.AddMergedRegion(new CellRangeAddress(
                     3, //first row (0-based)
                     3, //last row  (0-based)
-                    columNo++, //first column (0-based)
-                    columNo++  //last column  (0-based)
+                    firstColumnNo, //first column (0-based)
+                    columNo++ //last column  (0-based)
                 ));
             });
 
             row = sheet.CreateRow(4);
+
+            cell = row.CreateCell(0);
+            cell.CellStyle = styles["Header"];
+
+            cell = row.CreateCell(1);
+            cell.CellStyle = styles["Header"];
+
             columNo = 2;
             data.Sectors.ForEach(s => {
                 cell = row.CreateCell(columNo++);
@@ -227,6 +291,20 @@ namespace dica.Helper
                 cell.CellStyle = styles["Header"];
                 cell.SetCellValue("Qty");
             });
+
+            sheet.AddMergedRegion(new CellRangeAddress(
+                    3, //first row (0-based)
+                    4, //last row  (0-based)
+                    0, //first column (0-based)
+                    0  //last column  (0-based)
+            ));
+
+            sheet.AddMergedRegion(new CellRangeAddress(
+                    3, //first row (0-based)
+                    4, //last row  (0-based)
+                    1, //first column (0-based)
+                    1  //last column  (0-based)
+            ));
         }
 
         private static void CreateRow(ISheet sheet, Dictionary<string, ICellStyle> styles, InvestmentByCountryViewModel data)
